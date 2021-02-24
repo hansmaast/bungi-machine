2780bea7527939861cde39fb651d8bc21cc1675e
Hans Maast <hfmaast@gmail.com>
2021-02-24 22:40:11 +0100

    Refactor, add change prop

--- src/components/SelectDrumSampler.tsx
+++ src/components/SelectDrumSampler.tsx
@@ -1,27 +1,37 @@
+import { Sampler } from 'tone';
 import { useGlobalState } from '../context/GlobalState';
 import { Button } from '../style/button';
 import { Flex } from '../style/flex';
 import { ACTIONS } from '../context/reducer';
 import { SAMPLERS } from '../instruments/samplers';
+import { Title } from '../style/title';
+
+const { TR808, CR78 } = SAMPLERS;
 
 export function SelectDrumSampler() {
-  const { dispatch } = useGlobalState();
+  const { state, dispatch } = useGlobalState();
+  const { selectedSampler } = state;
+  const handleSelection = (sampler :Sampler) => {
+    dispatch({
+      type: ACTIONS.SELECT_DRUM_PRESET,
+      payload: sampler,
+    });
+  };
+
+  console.log(state.selectedSampler.name);
   return (
-    <Flex flexDirection="row">
-      <Button onClick={() => dispatch({
-        type: ACTIONS.SELECT_DRUM_PRESET,
-        payload: SAMPLERS.TR808,
-      })}
-      >
-        TR808
-      </Button>
-      <Button onClick={() => dispatch({
-        type: ACTIONS.SELECT_DRUM_PRESET,
-        payload: SAMPLERS.CR78,
-      })}
-      >
-        CR78
-      </Button>
+    <Flex>
+      <Title fontSize={3} fontWeight={500}>
+        Select sampler
+      </Title>
+      <Flex flexDirection="row">
+        <Button isActive={selectedSampler.name === 'TR808'} onClick={() => handleSelection(TR808)}>
+          TR808
+        </Button>
+        <Button isActive={state.selectedSampler.name !== 'CR78'} onClick={() => handleSelection(CR78)}>
+          CR78
+        </Button>
+      </Flex>
     </Flex>
   );
 }
--- src/instruments/samplers.js
+++ src/instruments/samplers.js
@@ -8,10 +8,12 @@ import { TR808_URLS } from '../assets/drums/TR808';
 const CR78 = new Sampler({
   urls: CR78_URLS,
 }).toDestination();
+CR78.name = 'CR78';
 
 const TR808 = new Sampler({
   urls: TR808_URLS,
 }).toDestination();
+TR808.name = 'TR808';
 
 export const SAMPLERS = {
   CR78,
