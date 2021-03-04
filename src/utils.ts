8ff63e2a11eaaf0d869beee401ad37bbeb7413c7
Hans Maast <hfmaast@gmail.com>
2021-03-04 19:46:06 +0100

    Fix quotes

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
+import { IGlobalState } from './context/types';
+
 export function persistInLocalStorage(key: string, data: any) {
   const stringifiedData = JSON.stringify(data, null, 2);
   localStorage.setItem(key, stringifiedData);
 }
 
-export function retrieveFromLocalStorage(key: string): any {
+export function retrieveFromLocalStorage(key: string): IGlobalState {
   let data;
   if (key) {
     data = localStorage.getItem(key);
